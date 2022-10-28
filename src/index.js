af76e240b43aaf9a5df4d36f39b32632bbb243f5
Sam <samhkcho@gmail.com>
2022-10-28 04:05:30 -0400

    remove unused var

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
