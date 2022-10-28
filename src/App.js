b6689a84fe7324c24af87932f8499ee950a2c335
Sam <samhkcho@gmail.com>
2022-10-28 04:48:27 -0400

    remove fall backs

--- src/App.js
+++ src/App.js
@@ -31,10 +31,8 @@ export default function App() {
           {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-          <Route path="*" element={<NoMatch />} />
         </Route>
         <Route index element={<Home />} />
-        <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
   );
