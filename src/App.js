374c37fc98ec167755fe28c6ea1b7129ddec30fc
Sam <samhkcho@gmail.com>
2022-10-28 05:27:44 -0400

    add star

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ export default function App() {
           {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
+          <Route path="*" element={<NoMatch />} />
         </Route>
         <Route path="*" element={<NoMatch />} />
       </Routes>
